22941baccc5508030eba3442f968a1c5f40126a6
Vishnu S <vishnus510412@gmail.com>
2025-02-05 23:13:42 +0530

    feat: WIP: Prisma Schema Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 import Link from "next/link";
 
 export default function Home() {
